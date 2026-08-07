14cb2d1207c253da116d75ca25d4e139c7c71283
takeokunn <bararararatty@gmail.com>
2026-08-07 15:17:44 +0900

    fix(test): avoid cl-weave package name conflicts

--- t/package.lisp
+++ t/package.lisp
@@ -1,4 +1,8 @@
 ;;;; t/package.lisp — test package for cl-cc-mir
 
 (defpackage :cl-cc-mir/test
-  (:use :cl :cl-weave))
+  (:use :cl)
+  (:import-from :cl-weave
+                :describe-sequential
+                :it
+                :expect))
